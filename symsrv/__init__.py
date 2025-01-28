8a314f20c08b043094d1a13598cbf4cb2bd22230
Steven Noonan <steven@uplinklabs.net>
2025-01-28 13:33:08 -0800

    add pyinstrument request profiling support
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- symsrv/__init__.py
+++ symsrv/__init__.py
@@ -28,6 +28,7 @@ TTL_SUCCESS_DEFAULT = 8 * 60 * 60  # 8 hours
 TTL_FAILURE_DEFAULT = 1 * 60 * 60  # 1 hour
 
 CHUNK_SIZE = 256 * 1024
+PROFILING_ENABLED = False
 
 cache_ttl: dict[int, int] = {}
 
@@ -39,6 +40,49 @@ suffix_blacklist: list[str] = []
 ignore_access: set[typing.Dict[str, typing.Any]] = set()
 
 app = FastAPI()
+
+def register_middleware(app: FastAPI):
+    if PROFILING_ENABLED == True:
+        from pyinstrument import Profiler
+        from pyinstrument.renderers.html import HTMLRenderer
+        from pyinstrument.renderers.speedscope import SpeedscopeRenderer
+
+        @app.middleware("http")
+        async def profile_request(request: Request, call_next: typing.Callable):
+            """Profile the current request
+
+            Taken from https://pyinstrument.readthedocs.io/en/latest/guide.html#profile-a-web-request-in-fastapi
+            with small improvements.
+
+            """
+            # we map a profile type to a file extension, as well as a pyinstrument profile renderer
+            profile_type_to_ext = {"html": "html", "speedscope": "speedscope.json"}
+            profile_type_to_renderer = {
+                "html": HTMLRenderer,
+                "speedscope": SpeedscopeRenderer,
+            }
+
+            # if the `profile=true` HTTP query argument is passed, we profile the request
+            if request.query_params.get("profile", False):
+
+                # The default profile format is speedscope
+                profile_type = request.query_params.get("profile_format", "speedscope")
+
+                # we profile the request along with all additional middlewares, by interrupting
+                # the program every 1ms1 and records the entire stack at that point
+                with Profiler(interval=0.001, async_mode="enabled") as profiler:
+                    response = await call_next(request)
+
+                # we dump the profiling into a file
+                extension = profile_type_to_ext[profile_type]
+                renderer = profile_type_to_renderer[profile_type]()
+                with open(f"profile.{extension}", "w") as out:
+                    out.write(profiler.output(renderer=renderer))
+                return response
+
+            # Proceed without profiling
+            return await call_next(request)
+register_middleware(app)
 
 class FilteredAccessLog(logging.Filter):
     def __init__(self, *args, **kwargs):
